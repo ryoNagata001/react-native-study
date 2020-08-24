bef5a0efc2f9fd3dbf824770bbb43c4855ba670d
ryo.nagata <ryo.nagata@chillnn.com>
2020-08-25 08:24:47 +0900

    update: state

---
+++ store/actions/user.js
@@ -0,0 +1,17 @@
+export const addClip = ({
+    clip
+}) => {
+    return {
+        type: 'ADD_CLIP',
+        clip
+    }
+}
+
+export const deleteClip = ({
+    clip
+}) => {
+    return {
+        type: 'DELETE_CLIP',
+        clip
+    }
+}
