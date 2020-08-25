f01928eac9d4a7ca549a5a6b9218bb59c833909b
ryo.nagata <ryo.nagata@chillnn.com>
2020-08-25 21:50:46 +0900

    done

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { FlatList, SafeAreaView } from 'react-native';
 import ListItem from '../components/ListItem';
+import Loading from '../components/Loading';
 import Constants from 'expo-constants';
 import axios from 'axios';
 
@@ -8,18 +9,21 @@ const URL = `http://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.m
 
 export default HomeScreen = ({ navigation }) => {
     const [articles, setArticles] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         fetchArticles();
     }, []);
 
     const fetchArticles = async () => {
+        setLoading(true);
         try {
             const response = await axios.get(URL);
             setArticles(response.data.articles);
         } catch (err) {
             console.error(err);
         }
+        setLoading(false);
     };
 
     return (
@@ -40,6 +44,7 @@ export default HomeScreen = ({ navigation }) => {
                 )}
                 keyExtractor={(_, index) => index.toString()}
             ></FlatList>
+            {loading && <Loading />}
         </SafeAreaView>
     );
 };
